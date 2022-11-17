942fe98d0cfdc6e697adc09fc386877afa0f0536
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2022-11-18 01:23:42 +0900

    test

--- enrolment/models.py
+++ enrolment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# DB에 관한 내용들
+# 태이블 생성할때마다   python manage.py makemigrations
+#                    python manage.py migrate
+# 그리고 admin.py에서 admin.site.register(User) 해줘야댐
+
+class User(models.Model):
+    # 컬럼명 : 타입()
+    userid = models.TextField()
--- enrolment/views.py
+++ enrolment/views.py
@@ -1,3 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+class infolist(APIView):
+    def get(self, request):
+        # DB에 접근하는 코드
+        # 요청변수를 추출하여 변수에 저장
+        id = request.query_params.get('userid')
+
+        user = User.objects.get(userid=id)
+
+        return Response({
+            "d":user.userid
+        })
+
+    # setting에 restframework 넣어주어야함
