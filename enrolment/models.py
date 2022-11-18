d1c0ebca0d14767c828615dd11f2d0797e47bcaa
Yujin_nKim <yujin.kimd@gmail.com>
2022-11-19 00:38:17 +0900

    feature: create Course table

--- enrolment/models.py
+++ enrolment/models.py
@@ -11,16 +11,15 @@ class Student(models.Model): #학생 테이블(PK = 학번)
     credit = models.IntegerField() #현재 수강 학점
     majorName = models.TextField() #학과
 
-# class Course(models.Model): #강의 테이블(PK = 과목번호, 분반)
-#     subjectNumber = models.TextField(primary_key=True) #과목 번호
-#     classNumber = models.TextField(primary_key=True) #분반
-#     subjectName = models.TextField() #과목명
-#     majorName = models.TextField() #학과명
-#     grade = models.IntegerField() #대상 학년
-#     credit = models.IntegerField() #이수 학점
-#     professorName = models.TextField() #교수명
-#     limitNumber = models.IntegerField() #정원
-#     currentNumber = models.IntegerField() #현재 수강 신청 인원
+class Course(models.Model): #강의 테이블(PK = 과목번호, 분반)
+    courseNumber = models.TextField(primary_key=True) #학수 번호(과목 번호 + 분반)
+    subjectName = models.TextField() #과목명
+    majorName = models.TextField() #학과명
+    grade = models.IntegerField() #대상 학년
+    credit = models.IntegerField() #이수 학점
+    professorName = models.TextField() #교수명
+    limitNumber = models.IntegerField() #정원
+    currentNumber = models.IntegerField() #현재 수강 신청 인원
 #
 # class ApplyCourse(models.Model):
 #     studentNumber = models.ForeignKey(Student)
