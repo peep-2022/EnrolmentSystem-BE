f68f4492c38850e9fff16d211598376c7083357f
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2022-11-19 15:36:03 +0900

    temp

--- enrolment/models.py
+++ enrolment/models.py
@@ -5,15 +5,3 @@ from django.db import models
 # 태이블 생성할때마다   python manage.py makemigrations
 #                    python manage.py migrate
 # 그리고 admin.py에서 admin.site.register(User) 해줘야댐
-
-class Class(models.Model):
-    # 컬럼명 = 타입()
-    majorName = models.TextField(default='대학 학과공통')
-    grade = models.TextField(default='')
-    credit = models.TextField(default='')
-    subjectName = models.TextField(default='')
-    subjectNumber = models.TextField(default='')
-    classNumber = models.TextField(default='')
-    professorName = models.TextField(default='')
-    limitNumber = models.TextField(default='0')
-    currentNumber = models.TextField(default='')
