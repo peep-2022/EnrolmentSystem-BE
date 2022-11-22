5d8afa912e7fba9463b47627296a60b66f75d5a4
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2022-11-23 01:04:41 +0900

    Update views.py
    reset element which can searchable
    remove credit & classNum & currentNum & limitNum
    and edit None option of grade

--- enrolment/views.py
+++ enrolment/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
 from .models import Course
+import urllib
 
 
 # Create your views here.
@@ -10,34 +11,58 @@ class searchList(APIView):
     def get(self, request):
         # DB에 접근하는 코드
         # 요청변수를 추출하여 변수에 저장
+
         _majorName = request.query_params.get('majorName')
         _grade = request.query_params.get('grade')
-        _credit = request.query_params.get('credit')
         _professorName = request.query_params.get('professorName')
         _subjectName = request.query_params.get('subjectName')
         _courseNumber = request.query_params.get('courseNumber')
-        _classNumber = request.query_params.get('classNumber')
-        _currentNumber = request.query_params.get('currentNumber')
-        _limitNumber = request.query_params.get('limitNumber')
 
-        if _majorName == '대학 학과공통':
-            cls = Course.objects.filter(grade=_grade, credit=_credit, subjectName=_subjectName,
-                                        courseNumber=_courseNumber, professorName=_professorName,
-                                        limitNumber=_limitNumber, currentNumber=_currentNumber).values()
-        else:
-            cls = Course.objects.filter(majorName=_majorName, grade=_grade, credit=_credit, subjectName=_subjectName,
-                                        courseNumber=_courseNumber, professorName=_professorName,
-                                        limitNumber=_limitNumber, currentNumber=_currentNumber).values()
+        urllib.parse.unquote(_subjectName)
+        urllib.parse.unquote(_majorName)
+        urllib.parse.unquote(_professorName)
 
-        return_list = list()
-        for item in return_list :
-            return_list.append(item)
+        if _majorName.find('대학 학과공통') > -1 :
+            cls = Course.objects.all()
+        else :
+            cls = Course.objects.filter(majorName=_majorName)
+        print(cls.count())
+        # courseNumber가 있을경우 filter
+        try :
+            if _courseNumber != 'None':
+                print("courseNumber not None")
+                cls = cls.filter(courseNumber=_courseNumber)
+            print(cls.count())
 
-        if return_list :
+            # grade가 있을경우 filter
+            if _grade != '0' :
+                print("grade None")
+                cls = cls.filter(grade=_grade)
+            print(cls.count())
+            # professorName가 있을경우 filter 해당 문자열으로 시작하는 값 / 끝나는 값 / 포함하는 값을 모두 포함하게 검색
+            if _professorName != 'None':
+                print("professorName not None")
+                cls = cls.filter(professorName__istartswith=_professorName) \
+                      | cls.filter(professorName__iendswith=_professorName) \
+                      | cls.filter(professorName__icontains=_professorName)
+            print(cls.count())
+            # currentNumber가 있을경우 filter
+            if _subjectName != 'None':
+                print("subjectName not None")
+                cls = cls.filter(subjectName__istartswith=_subjectName) \
+                      | cls.filter(subjectName__iendswith=_subjectName) \
+                      | cls.filter(subjectName__icontains=_subjectName)
+            print(cls.count())
+            if cls : # 필터링된 value가 있는경우 response해준다
+                print("return response")
+                return Response(cls.values())
+
             return Response(cls)
-        return Response({
-            'returnCode' : 'NoneError'
-        })
+        except:
+            return Response({
+                'returnCode': 'NoneError'
+            })
+
 
 
     # setting에 restframework 넣어주어야함
